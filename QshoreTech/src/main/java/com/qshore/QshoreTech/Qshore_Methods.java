c48d0b7db6a50f630a2a2bf344cec88b8ca04612
kishore.k1024@gmail.com <kishore.k1024@gmail.com>
2020-04-05 11:17:43 +0530

    C2

--- QshoreTech/src/main/java/com/qshore/QshoreTech/Qshore_Methods.java
+++ QshoreTech/src/main/java/com/qshore/QshoreTech/Qshore_Methods.java
@@ -4,11 +4,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Qshore_Methods {
-	
+	public WebDriver driver;
 	public void launchApp(String url){		
-		WebDriver driver=new FirefoxDriver();
+	       driver=new FirefoxDriver();
 		driver.get(url);
 		driver.manage().window().maximize();
 	}
+	public void closeApp(){
+		driver.close();
+	}
+
 
 }
